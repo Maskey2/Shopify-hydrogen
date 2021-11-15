dbcc7f55887ba3940453e955f34b11f40f7be939
nishma2 <nishma@sldrigging.com>
2021-11-15 11:25:47 -0600

    config store

--- shopify.config.js
+++ shopify.config.js
@@ -1,6 +1,7 @@
 export default {
   locale: 'en-us',
-  storeDomain: 'hydrogen-preview.myshopify.com',
-  storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+  storeDomain: 'rigging-store.myshopify.com/',
+  // storefrontToken: 'ed5f67b9d7e0cc10e37f6ba4b0248a9f',
+  storefrontToken: '2a113ae469e99ff3f0b9db054d5eb6aa',
   graphqlApiVersion: 'unstable',
 };
